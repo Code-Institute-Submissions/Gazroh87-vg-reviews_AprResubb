0ac5a616a9b232dd540c276668a8f93b9a907637
Gary Hughes <gary.hughes87@virginmedia.com>
2021-02-08 22:13:31 +0000

    Setup basic app route view functions for reviews, games, genres and platforms

--- app.py
+++ app.py
@@ -1,15 +1,47 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Flask is working"
+@app.route("/home")
+def home():
+    genres = mongo.db.genres.find()
+    return "Flask is working" + str(mongo.db.genres.find_one())
+    # return render_template("home.html", genres=genres, platforms=platforms)
+
+
+@app.route("/get_games")
+def get_games():
+    return render_template("games.html", games=games)
+
+
+@app.route("/get_reviews")
+def get_reviews():
+    return render_template("reviews.html", reviews=reviews)
+
+
+@app.route("/get_genres")
+def get_genres():
+    return render_template("genres.html", genres=genres)
+
+
+@app.route("/get_platforms")
+def get_platforms():
+    return render_template("platforms.html", platforms=platforms)
 
 
 if __name__ == "__main__":
