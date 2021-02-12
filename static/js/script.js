522a0dc82299f28045cb5cdeffc2a39fcfb5ace2
Gary Hughes <gary.hughes87@virginmedia.com>
2021-02-12 22:48:21 +0000

    Testing carousel feature with Python integration

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
      $('.sidenav').sidenav({ edge: "right", draggable: true });
-     $('.parallax').parallax();
      $(':header').css("font-family","'Press Start 2P'", "cursive")
+     $('.parallax').parallax();
+     $('.carousel').carousel();
      $("#copyright").text(new Date().getFullYear());
   });
